e3a95f705cc5604c12386fb5d1c68f35d37818a9
MayNiklas <info@niklas-steffen.de>
2021-09-11 18:35:41 +0200

    owncast: init at 0.0.8

---
+++ pkgs/servers/owncast/default.nix
@@ -0,0 +1,43 @@
+{ lib, buildGoModule, fetchFromGitHub, bash, which, ffmpeg, makeWrapper, ... }:
+
+buildGoModule rec {
+
+  pname = "owncast";
+  version = "0.0.8";
+
+  src = fetchFromGitHub {
+    owner = "owncast";
+    repo = "owncast";
+    rev = "v${version}";
+    sha256 = "0md4iafa767yxkwh6z8zpcjv9zd79ql2wapx9vzyd973ksvrdaw2";
+  };
+
+  vendorSha256 = "sha256-bH2CWIgpOS974/P98n0R9ebGTJ0YoqPlH8UmxSYNHeM=";
+
+  propagatedBuildInputs = [ ffmpeg ];
+
+  buildInputs = [ makeWrapper ];
+
+  postInstall = ''
+    wrapProgram $out/bin/owncast --prefix PATH : ${
+      lib.makeBinPath [ bash which ffmpeg ]
+    }
+
+    # TODO: copy in static and webroot directories
+  '';
+
+  installCheckPhase = ''
+    runHook preCheck
+    $out/bin/owncast --help
+    runHook postCheck
+  '';
+
+  meta = with lib; {
+    description = "self-hosted video live streaming solution";
+    homepage = "https://owncast.online";
+    license = licenses.mit;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ mayniklas ];
+  };
+
+}
