5f7e0bef29a7fcee2c72980f40cadf7a6dacabec
Jan Solanti <jhs@psonet.com>
2023-10-18 00:26:12 +0300

    pocl: init at 4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23649,6 +23649,8 @@ with pkgs;
 
   plib = callPackage ../development/libraries/plib { };
 
+  pocl = callPackage ../development/libraries/pocl { };
+
   poco = callPackage ../development/libraries/poco { };
 
   podofo = callPackage ../development/libraries/podofo { };
