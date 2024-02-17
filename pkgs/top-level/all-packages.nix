b3425fd971a13d61d12e5c7b9206d5ee5168eeb1
Rodney Lorrimar <dev@rodney.id.au>
2024-02-17 14:42:21 +0800

    saxon-he: 11.5 -> 12.4
    Adds `saxon_11-he` and `saxon_12-he` attributes for specific versions,
    and sets `saxon-he = saxon_12-he`.
    
    Adds an updateScript for the latest versions of Saxon-HE.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24967,6 +24967,7 @@ with pkgs;
   mockobjects = callPackage ../development/libraries/java/mockobjects { };
 
   saxonb = saxonb_8_8;
+  saxon-he = saxon_12-he;
 
   inherit (callPackages ../development/libraries/java/saxon {
     jre = jre_headless;
@@ -24976,7 +24977,8 @@ with pkgs;
     saxonb_8_8
     saxonb_9_1
     saxon_9-he
-    saxon-he;
+    saxon_11-he
+    saxon_12-he;
 
   smack = callPackage ../development/libraries/java/smack { };
 
