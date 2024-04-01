096771ceda06f699873c8c5e3ec40d0b9a82ff35
Andrew Marshall <andrew@johnandrewmarshall.com>
2024-04-01 11:37:34 -0400

    linux: enable BUG_ON_DATA_CORRUPTION
    Enabled in [Debian][1], [Fedora][2]; not in Arch. Recommended by [Kernel
    Self Protection Project][3].
    
    [2]: https://salsa.debian.org/kernel-team/linux/-/blob/5f6aa5cb48d261cef1f31ec18f969c986fd3bd77/debian/config/config#L7719
    [3]: https://src.fedoraproject.org/rpms/kernel/blob/e8025dc0fba7e18c74f529a93d653c71d16e5a56/f/kernel-x86_64-fedora.config#_789
    [4]: https://kernsec.org/wiki/index.php?title=Kernel_Self_Protection_Project/Recommended_Settings&oldid=4078

--- pkgs/os-specific/linux/kernel/common-config.nix
+++ pkgs/os-specific/linux/kernel/common-config.nix
@@ -673,6 +673,7 @@ let
     security = {
       # Report BUG() conditions and kill the offending process.
       BUG = yes;
+      BUG_ON_DATA_CORRUPTION = yes;
 
       FORTIFY_SOURCE                   = option yes;
 
