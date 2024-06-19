264ff04b97189ff337b679b0050bb538b8e77e31
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:06:22 +0200

    python3Packages.manim-slides: 5.1.6 -> 5.1.7
    https://github.com/jeertmans/manim-slides/blob/refs/tags/v5.1.7/CHANGELOG.md

--- pkgs/development/python-modules/manim-slides/default.nix
+++ pkgs/development/python-modules/manim-slides/default.nix
@@ -35,7 +35,7 @@
 }:
 buildPythonPackage rec {
   pname = "manim-slides";
-  version = "5.1.6";
+  version = "5.1.7";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -44,7 +44,7 @@ buildPythonPackage rec {
     owner = "jeertmans";
     repo = "manim-slides";
     rev = "refs/tags/v${version}";
-    hash = "sha256-BFfp/jeKBXr+Ukh0jNEtMPHlstlBjhSnvA803ddYoZE=";
+    hash = "sha256-egQYL4Qvs1fQcJ5WEM461TSYqs2XN39wbyfx9uEFeIs=";
   };
 
   build-system = [
