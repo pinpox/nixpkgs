6bc4ab32cc39ce2a7388e2d99a5161a824f84365
José Romildo <malaquias@gmail.com>
2024-07-01 10:05:52 -0300

    marwaita: move to pkgs/by-name/ma/

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28584,8 +28584,6 @@ with pkgs;
 
   martian-mono = callPackage ../data/fonts/martian-mono { };
 
-  marwaita = callPackage ../data/themes/marwaita { };
-
   marwaita-icons = callPackage ../by-name/ma/marwaita-icons/package.nix {
     inherit (kdePackages) breeze-icons;
   };
